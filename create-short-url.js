d46209bc029643527fbeb6a52534d63ea2e0ba49
Adarsh singh <adarshsingh0930@gmail.com>
2024-05-10 09:47:57 +0530

    removed unnecessary lines

--- create-short-url.js
+++ create-short-url.js
@@ -3,7 +3,6 @@ const {writeToDb} = require('./database.js')
 
 async function createShortUrl (url) { 
     const result = await writeToDb(url)
-    console.log(result)
     const uniqueID = result.insertedId
     return `http://localhost:4000/${uniqueID}`
 }
